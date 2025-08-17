06f2e0c62a9f136ab6825ff88e42ba3c48ac6b1e
Marina Barros <marina.barros@acad.pucrs.br>
2025-08-17 20:29:18 -0300

    Module7

--- app/src/Main.java
+++ app/src/Main.java
@@ -1,8 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public void main(String[] args) {
 
+        module7();
+    }
+
+    public void module7() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please insert an int: ");
+        int numOne = scanner.nextInt();
+
+        System.out.println("Please insert another int: ");
+        int numTwo = scanner.nextInt();
+
+        if (numOne > numTwo) {
+            System.out.println("Num 1 is bigger" + numOne);
+        } else if (numOne < numTwo) {
+            System.out.println("Num 2 is bigger: " + numTwo);
+        } else System.out.println("Both are the same number");
+
+
+    }
+
+    public void module5() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, welcome!");
 
@@ -21,8 +43,6 @@ public class Main {
         System.out.println("Please insert your height: ");
         float height = scanner.nextFloat();
 
-        System.out.println("Data input: " + name+ " " + cpf + " " + age + " " + weight + " " + " " + height);
-
-
+        System.out.println("Data input: " + name + " " + cpf + " " + age + " " + weight + " " + " " + height);
     }
 }
