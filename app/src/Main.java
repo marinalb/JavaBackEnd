9d48d33cd434bdf03f7f6bf091ec5282aa986bfb
Marina Barros <marina.barros@acad.pucrs.br>
2025-08-18 09:19:04 -0300

    Module8

--- app/src/Main.java
+++ app/src/Main.java
@@ -3,8 +3,42 @@ import java.util.Scanner;
 public class Main {
     public void main(String[] args) {
 
-        module7();
+        module8();
     }
+
+    public void module8() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please, insert your name: ");
+        String nome = sc.nextLine();
+        System.out.println("Insert a number: ");
+
+        while (!sc.hasNextInt()) {
+            System.out.println("Needs to be a number");
+            sc.next();
+        }
+        int num = sc.nextInt();
+
+        for(int i=0; i <= num; i++) {
+            System.out.println(i);
+        }
+
+        for(int i = num; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+      if (nome.length() > 6) {
+          System.out.println(nome);
+      }else {
+          for (int i=0; i< nome.length(); i++) {
+              System.out.println(nome);
+          }
+      }
+    }
+
+
+
 
     public void module7() {
         Scanner scanner = new Scanner(System.in);
